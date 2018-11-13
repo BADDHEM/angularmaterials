350bfa32fa8398e80a8177abbb0dcb2c991eb154
vamsi <tovamsich@gmail.com>
2018-11-13 21:07:33 +0530

    added matgerilas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/material.module';
+import { Routes } from '@angular/router';
+
+const routes:Routes = [
+{ path:'contactmanager',loadChildren:'./contactmanager/contactmanager.module#ContactmanagerModule'},
+{path:'**',redirectTo:'contactmanager'}
+]
+
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
